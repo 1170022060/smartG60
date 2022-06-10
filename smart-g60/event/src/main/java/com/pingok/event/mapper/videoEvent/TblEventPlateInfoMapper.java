19c4faa4c145486c21f1dab0d73ce93ba72f37aa
1991566969@qq.com <1991566969@qq.com>
2022-06-10 18:31:29 +0800

    算法接口：根据桩号、车道号查询车道平均速度

--- smart-g60/event/src/main/java/com/pingok/event/mapper/videoEvent/TblEventPlateInfoMapper.java
+++ smart-g60/event/src/main/java/com/pingok/event/mapper/videoEvent/TblEventPlateInfoMapper.java
@@ -2,8 +2,23 @@ package com.pingok.event.mapper.videoEvent;
 
 import com.pingok.event.domain.videoEvent.TblEventPlateInfo;
 import com.ruoyi.common.core.mapper.CommonRepository;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface TblEventPlateInfoMapper extends CommonRepository<TblEventPlateInfo> {
+    @Select({"<script>" +
+            "select UBI_SOURCE_ID as \"ubiSourceId\", UI_SPEED as \"uiSpeed\" from (" +
+            "select ROW_NUMBER()over(PARTITION by UBI_SOURCE_ID ORDER BY UBI_SOURCE_ID desc) as \"sid\", UBI_SOURCE_ID, UI_SPEED " +
+            "from TBL_EVENT_PLATE_INFO WHERE UI_VEHICLE_LANE = #{laneNo}" +
+            ") dual where UBI_SOURCE_ID in " +
+            "<foreach collection = \"camIds\" index=\"index\" item=\"camId\" open=\"(\" separator=\",\" close=\")\">" +
+            " #{camId}" +
+            "</foreach>" +
+            "</script>"})
+    List<TblEventPlateInfo> selectByLane(@Param("laneNo") Integer laneNo, @Param("camIds") List<String> camIds);
+
 }
