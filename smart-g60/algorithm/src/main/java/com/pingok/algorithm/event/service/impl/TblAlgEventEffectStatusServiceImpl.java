bf5459ad8e73cfbbc11bee1a4a3b0b64e9fde7ed
sunshaoyu0421 <qq975117270>
2022-06-14 10:52:47 +0800

    新增获取车道平均速度接口

--- smart-g60/algorithm/src/main/java/com/pingok/algorithm/event/service/impl/TblAlgEventEffectStatusServiceImpl.java
+++ smart-g60/algorithm/src/main/java/com/pingok/algorithm/event/service/impl/TblAlgEventEffectStatusServiceImpl.java
@@ -1,5 +1,6 @@
 package com.pingok.algorithm.event.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.pingok.algorithm.event.domain.StationLineRecordDto;
 import com.pingok.algorithm.event.entity.TblAlgEvent;
 import com.pingok.algorithm.event.entity.TblAlgEventEffectStatus;
@@ -7,9 +8,12 @@ import com.pingok.algorithm.event.mapper.TblAlgEventEffectStatusMapper;
 import com.pingok.algorithm.event.service.TblAlgEventEffectStatusService;
 import com.pingok.algorithm.event.service.TblAlgEventService;
 import com.pingok.algorithm.trafficStatus.mapper.TblAlgTrafficStatusSpeedMapper;
+import com.ruoyi.common.core.domain.R;
 import com.ruoyi.common.core.utils.DateUtils;
 import com.ruoyi.common.core.utils.StringUtils;
+import com.ruoyi.system.api.RemoteEventService;
 import com.ruoyi.system.api.RemoteIdProducerService;
+import com.ruoyi.system.api.domain.algorithm.LaneAvgSpeed;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -35,6 +39,9 @@ public class TblAlgEventEffectStatusServiceImpl implements TblAlgEventEffectStat
 
     @Resource
     private RemoteIdProducerService remoteIdProducerService;
+
+    @Resource
+    private RemoteEventService remoteEventService;
 
     /**
      * 查询事件影响状态列表
@@ -98,51 +105,59 @@ public class TblAlgEventEffectStatusServiceImpl implements TblAlgEventEffectStat
             if (DateUtils.compareTo(currentTimeStr, tblAlgEvent.getStartTime())){
                 // 更新事件执行中
                 tblAlgEvent.setStatus(1);
-                // TODO 根据桩号、车道号查询车道平均速度
-                List<StationLineRecordDto> stationLineRecordDtoList = new ArrayList<>();
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("mileage", tblAlgEvent.getStationNum());
+                jsonObject.put("laneNo", tblAlgEvent.getLineNum());
+                jsonObject.put("dir", tblAlgEvent.getTrafficDirection());
+                R<List<LaneAvgSpeed>> result = remoteEventService.getLaneAvgSpeed(jsonObject);
+                if (result == null){
+                    continue;
+                }
+                // 根据桩号、车道号查询车道平均速度
+                List<LaneAvgSpeed> laneAvgSpeedList = result.getData();
                 // 根据事件id查询第一条影响状态记录
                 TblAlgEventEffectStatus firstEventEffectStatus = algEventEffectStatusMapper.getFirstEventEffectStatusByEventId(tblAlgEvent.getId());
                 // 产生影响
-                StationLineRecordDto stationLineRecordDto = getStationLineRecord(stationLineRecordDtoList, firstEventEffectStatus);
+                LaneAvgSpeed laneAvgSpeed = getStationLineRecord(laneAvgSpeedList, firstEventEffectStatus);
                 // 速度变化率
                 double speedRate = 0.0;
-                if (stationLineRecordDto != null && stationLineRecordDto.getIsEffect() == 1){
+                if (laneAvgSpeed != null && laneAvgSpeed.getIsEffect() == 1){
                     // 根据事件id查询最新影响状态记录
                     TblAlgEventEffectStatus latestEventEffectStatus = algEventEffectStatusMapper.getLatestEventEffectStatusByEventId(tblAlgEvent.getId());
                     // 计算速度变化率
-                    speedRate = (Double.parseDouble(latestEventEffectStatus.getLineSpeed()) - Double.parseDouble(stationLineRecordDto.getLineSpeed())) / 2;
+                    speedRate = (Double.parseDouble(latestEventEffectStatus.getLineSpeed()) - Double.parseDouble(laneAvgSpeed.getLineSpeed())) / 2;
                     // 获取该限速下轻度拥堵、中度拥堵、重度拥堵速度
-                    int mildCongSpeed = algTrafficStatusSpeedMapper.getMaxSpeed(stationLineRecordDto.getLimitSpeed(), 1);
-                    int medCongSpeed = algTrafficStatusSpeedMapper.getMaxSpeed(stationLineRecordDto.getLimitSpeed(), 2);
-                    int sevCongSpeed = algTrafficStatusSpeedMapper.getMaxSpeed(stationLineRecordDto.getLimitSpeed(), 3);
+                    int mildCongSpeed = algTrafficStatusSpeedMapper.getMaxSpeed(laneAvgSpeed.getLimitSpeed(), 1);
+                    int medCongSpeed = algTrafficStatusSpeedMapper.getMaxSpeed(laneAvgSpeed.getLimitSpeed(), 2);
+                    int sevCongSpeed = algTrafficStatusSpeedMapper.getMaxSpeed(laneAvgSpeed.getLimitSpeed(), 3);
                     Calendar calendar = Calendar.getInstance();
                     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                     // 如果当前速度低于轻度拥堵速度
-                    if (Double.parseDouble(stationLineRecordDto.getLineSpeed()) <= mildCongSpeed){
+                    if (Double.parseDouble(laneAvgSpeed.getLineSpeed()) <= mildCongSpeed){
                         tempEventEffectStatus.setMildCongTime("0");
                     } else {
                         // 车道速度降低到轻度拥堵速度的时长（min）
-                        int mildCongDuration = new BigDecimal((Double.parseDouble(stationLineRecordDto.getLineSpeed()) - mildCongSpeed) / speedRate).setScale(0, RoundingMode.CEILING).intValue();
+                        int mildCongDuration = new BigDecimal((Double.parseDouble(laneAvgSpeed.getLineSpeed()) - mildCongSpeed) / speedRate).setScale(0, RoundingMode.CEILING).intValue();
                         calendar.setTime(currentDate);
                         calendar.add(Calendar.MINUTE, mildCongDuration);
                         String mildCongTime = simpleDateFormat.format(calendar.getTime());
                         tempEventEffectStatus.setMildCongTime(mildCongTime);
                     }
-                    if (Double.parseDouble(stationLineRecordDto.getLineSpeed()) <= medCongSpeed){
+                    if (Double.parseDouble(laneAvgSpeed.getLineSpeed()) <= medCongSpeed){
                         tempEventEffectStatus.setMedCongTime("0");
                     } else {
                         // 车道速度降低到中度拥堵速度的时长（min）
-                        int medCongDuration = new BigDecimal((Double.parseDouble(stationLineRecordDto.getLineSpeed()) - medCongSpeed) / speedRate).setScale(0, RoundingMode.CEILING).intValue();
+                        int medCongDuration = new BigDecimal((Double.parseDouble(laneAvgSpeed.getLineSpeed()) - medCongSpeed) / speedRate).setScale(0, RoundingMode.CEILING).intValue();
                         calendar.setTime(currentDate);
                         calendar.add(Calendar.MINUTE, medCongDuration);
                         String medCongTime = simpleDateFormat.format(calendar.getTime());
                         tempEventEffectStatus.setMedCongTime(medCongTime);
                     }
-                    if (Double.parseDouble(stationLineRecordDto.getLineSpeed()) <= sevCongSpeed){
+                    if (Double.parseDouble(laneAvgSpeed.getLineSpeed()) <= sevCongSpeed){
                         tempEventEffectStatus.setSevCongTime("0");
                     } else {
                         // 车道速度降低到重度拥堵速度的时长（min）
-                        int sevCongDuration = new BigDecimal((Double.parseDouble(stationLineRecordDto.getLineSpeed()) - sevCongSpeed) / speedRate).setScale(0, RoundingMode.CEILING).intValue();
+                        int sevCongDuration = new BigDecimal((Double.parseDouble(laneAvgSpeed.getLineSpeed()) - sevCongSpeed) / speedRate).setScale(0, RoundingMode.CEILING).intValue();
                         calendar.setTime(currentDate);
                         calendar.add(Calendar.MINUTE, sevCongDuration);
                         String sevCongTime = simpleDateFormat.format(calendar.getTime());
@@ -151,9 +166,9 @@ public class TblAlgEventEffectStatusServiceImpl implements TblAlgEventEffectStat
                 }
                 tempEventEffectStatus.setId(remoteIdProducerService.nextId());
                 tempEventEffectStatus.setEventId(tblAlgEvent.getId());
-                tempEventEffectStatus.setLineSpeed(stationLineRecordDto != null? stationLineRecordDto.getLineSpeed(): null);
+                tempEventEffectStatus.setLineSpeed(laneAvgSpeed != null? laneAvgSpeed.getLineSpeed(): null);
                 tempEventEffectStatus.setSampleTime(currentTimeStr);
-                tempEventEffectStatus.setEffectFlg(stationLineRecordDto != null? stationLineRecordDto.getIsEffect(): null);
+                tempEventEffectStatus.setEffectFlg(laneAvgSpeed != null? laneAvgSpeed.getIsEffect(): null);
                 tempEventEffectStatus.setCreateTime(currentDate);
                 algEventEffectStatusMapper.insert(tempEventEffectStatus);
 
@@ -174,25 +189,25 @@ public class TblAlgEventEffectStatusServiceImpl implements TblAlgEventEffectStat
 
     /**
      * 获取车道影响状态结果
-     * @param stationLineRecordDtoList 车道数据
+     * @param laneAvgSpeedList 车道数据
      * @param tblAlgEventEffectStatus 第一次采样记录
      * @return
      */
-    public StationLineRecordDto getStationLineRecord(List<StationLineRecordDto> stationLineRecordDtoList, TblAlgEventEffectStatus tblAlgEventEffectStatus){
-        if (stationLineRecordDtoList == null || stationLineRecordDtoList.size() == 0 || stationLineRecordDtoList.size() == 1){
+    public LaneAvgSpeed getStationLineRecord(List<LaneAvgSpeed> laneAvgSpeedList, TblAlgEventEffectStatus tblAlgEventEffectStatus){
+        if (laneAvgSpeedList == null || laneAvgSpeedList.size() == 0 || laneAvgSpeedList.size() == 1){
             return null;
         }
         // 当前车道速度
         Double currentLineSpeed = null;
         // 相邻车道速度
         List<Double> adjacentLineSpeed = new ArrayList<>();
-        StationLineRecordDto recordDto = new StationLineRecordDto();
-        for (StationLineRecordDto stationLineRecordDto : stationLineRecordDtoList) {
-            if (stationLineRecordDto.getLineType() == 0){
-                currentLineSpeed = Double.parseDouble(stationLineRecordDto.getLineSpeed());
-                recordDto = stationLineRecordDto;
+        LaneAvgSpeed recordDto = new LaneAvgSpeed();
+        for (LaneAvgSpeed laneAvgSpeed : laneAvgSpeedList) {
+            if (laneAvgSpeed.getLineType() == 0){
+                currentLineSpeed = Double.parseDouble(laneAvgSpeed.getLineSpeed());
+                recordDto = laneAvgSpeed;
             } else {
-                adjacentLineSpeed.add(Double.parseDouble(stationLineRecordDto.getLineSpeed()));
+                adjacentLineSpeed.add(Double.parseDouble(laneAvgSpeed.getLineSpeed()));
             }
         }
         if (currentLineSpeed == null){
