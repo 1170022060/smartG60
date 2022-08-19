b0359e1173d2187f00a73bb2efffbc20be086dc4
405741138@qq.com <qm880312>
2022-08-19 16:05:30 +0800

    data-center

--- smart-g60/vocational/src/main/java/com/pingok/vocational/service/emergency/impl/EventPalnServiceImpl.java
+++ smart-g60/vocational/src/main/java/com/pingok/vocational/service/emergency/impl/EventPalnServiceImpl.java
@@ -46,7 +46,7 @@ public class EventPalnServiceImpl implements IEventPalnService {
         TblEventRecord eventRecord = tblEventRecordMapper.selectByPrimaryKey(id);
         String[] gps = null;
         if (eventRecord.getLocationInterval() != null) {
-            gps = eventRecord.getLocationInterval().split(",");
+            gps = eventRecord.getLocationInterval().replace("[","").replace("]","").split(",");
         }
         String eventType = eventRecord.getEventType();
         if (eventType != null) {
