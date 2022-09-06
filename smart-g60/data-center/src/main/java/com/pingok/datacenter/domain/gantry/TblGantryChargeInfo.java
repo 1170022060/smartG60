4e46bfe29709424e9bc99e5a907191a74b54c405
maoyinan <920746980@qq.com>
2022-09-06 18:26:26 +0800

    临时提交

--- smart-g60/data-center/src/main/java/com/pingok/datacenter/domain/gantry/TblGantryChargeInfo.java
+++ smart-g60/data-center/src/main/java/com/pingok/datacenter/domain/gantry/TblGantryChargeInfo.java
@@ -17,5 +17,37 @@ public class TblGantryChargeInfo implements Serializable {
     @Id
     private Long id;
 
+    private String chargingUnitId;
 
+    private String chargingUnitName;
+
+    private Integer nature;     //路径性质：1-经营性；2-还贷性
+
+    private Integer direction;  //方向
+
+    private Integer mileage;
+
+    private Double startLongitude;
+
+    private Double startLatitude;
+
+    private String startPileNo;
+
+    private String endPileNo;
+
+    private Double endLongitude;
+
+    private Double endLatitude;
+
+//    private String coincidenceGB;   //重合收费公路编号
+
+    private Integer limitId;        //省界标识：0-非省界 1-省界
+
+    private Integer status;
+
+    private Integer chargingUnitType;   //1-收费区间；2-算法区间（带收费站）
+
+    private Integer limitSpeed;         //限速 km/h
+
+    private Double dailyIncomeFactor;   //理论日收益计算因子
 }
--- smart-g60/data-center/src/main/java/com/pingok/datacenter/service/gantry/impl/GantryAlgorithmImpl.java
+++ smart-g60/data-center/src/main/java/com/pingok/datacenter/service/gantry/impl/GantryAlgorithmImpl.java
@@ -1,10 +1,15 @@
 package com.pingok.datacenter.service.gantry.impl;
 
+import com.pingok.datacenter.domain.gantry.TblGantryChargeInfo;
 import com.pingok.datacenter.mapper.gantry.TblGantryAlgorithmMapper;
+import com.pingok.datacenter.mapper.gantry.TblGantryChargeInfoMapper;
 import com.pingok.datacenter.service.gantry.IGantryAlgorithm;
+import com.ruoyi.common.core.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +24,8 @@ public class GantryAlgorithmImpl implements IGantryAlgorithm {
 
     @Autowired
     private TblGantryAlgorithmMapper tblGantryAlgorithmMapper;
+    @Autowired
+    private TblGantryChargeInfoMapper tblGantryChargeInfoMapper;
 
     @Override
     public List<Map> selectGantryAlgorithm(String startTime, String endTime) {
@@ -33,5 +40,28 @@ public class GantryAlgorithmImpl implements IGantryAlgorithm {
     @Override
     public List<Map> selectGantryAlgorithmPassRecord(String gantryId, String startTime, String endTime) {
         return tblGantryAlgorithmMapper.selectGantryAlgorithmPassRecord(gantryId,startTime,endTime);
+    }
+
+    @Override
+    public List<TblGantryChargeInfo> selectChargeInfo(String chargeId) {
+        Example example = new Example(TblGantryChargeInfo.class);
+        Example.Criteria criteria = example.createCriteria();
+        List<TblGantryChargeInfo> list = new ArrayList<>();
+        if(!StringUtils.isEmpty(chargeId)) {
+            criteria.andEqualTo("chargingUnitId", chargeId);
+            TblGantryChargeInfo info = tblGantryChargeInfoMapper.selectOneByExample(example);
+            list.add(info);
+        } else {
+            list = tblGantryChargeInfoMapper.selectByExample(example);
+        }
+        return list;
+    }
+
+    @Override
+    public List<Map> selectChargeFlow(String chargeId, String time) {
+//        TBL_GANTRY_TRANSACTION_2022
+        String table = String.format("TBL_GANTRY_TRANSACTION_%s", time.substring(0, 4));
+//        String transDate = time.replace('-', ' ');
+        return tblGantryChargeInfoMapper.selectChargeFlow(table, chargeId, time);
     }
 }
