e676a99f2b8205346c470d388df08e3d6f14f336
clearboom7 <986899445@qq.com>
2022-09-13 15:26:06 +0800

    提交

--- smart-g60/vocational/src/main/java/com/pingok/vocational/mapper/parkingLot/TblParkingStatisticsMapper.java
+++ smart-g60/vocational/src/main/java/com/pingok/vocational/mapper/parkingLot/TblParkingStatisticsMapper.java
@@ -17,13 +17,15 @@ import java.util.Map;
 public interface TblParkingStatisticsMapper extends CommonRepository<TblParkingStatistics> {
 
     @Select("select tfi.ID as \"fieldId\"," +
-            "tfi.FIELD_NAME as \"fieldName\"," +
-            "tps.VEH_TYPE as \"vehType\"," +
-            "sum(tps.ENTER) as \"dailyCumulative\" from TBL_PARKING_STATISTICS tps " +
+            "tfi.FIELD_NAME as \"fieldName\" from TBL_PARKING_STATISTICS tps " +
             "left join TBL_FIELD_INFO tfi on tfi.ID=tps.FIELD_ID " +
             "where tps.DAY = #{date} " +
-            "group by tfi.ID,tfi.FIELD_NAME,tps.VEH_TYPE" )
+            "group by tfi.ID,tfi.FIELD_NAME" )
     List<Map> trafficSum(@Param("date") Date date);
+
+    @Select("select sum(ENTER) as \"dailyCumulative\" from TBL_PARKING_STATISTICS " +
+            "where FIELD_ID = #{fieldId} and DAY = #{date} and VEH_TYPE = #{vehType}")
+    Integer trafficDaily(@Param("date") Date date, @Param("fieldId") Long fieldId,@Param("vehType") Integer vehType);
 
     @Select("select HOUR as \"hour\"," +
             "CURRENT_NUM as \"current\"  from TBL_PARKING_STATISTICS " +
