be4732b62c2fd08d381e6185dd2091837f8442fb
lal <3055344019@qq.com>
2022-10-12 17:15:14 +0800

    厕所排班接口编写

--- smart-g60/monitor/src/main/java/com/pingok/monitor/service/smartToilet/impl/SmartToiletScheduleServiceImpl.java
+++ smart-g60/monitor/src/main/java/com/pingok/monitor/service/smartToilet/impl/SmartToiletScheduleServiceImpl.java
@@ -1,4 +1,55 @@
 package com.pingok.monitor.service.smartToilet.impl;
 
-public class SmartToiletScheduleServiceImpl {
+import com.pingok.monitor.domain.smartToilet.TblSmartToiletSchedule;
+import com.pingok.monitor.mapper.smartToilet.SmartToiletScheduleMapper;
+import com.pingok.monitor.service.smartToilet.ISmartToiletScheduleService;
+import com.ruoyi.common.core.utils.DateUtils;
+import com.ruoyi.common.security.utils.SecurityUtils;
+import com.ruoyi.system.api.RemoteIdProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SmartToiletScheduleServiceImpl implements ISmartToiletScheduleService {
+
+    @Autowired
+    private RemoteIdProducerService remoteIdProducerService;
+
+    @Autowired
+    private SmartToiletScheduleMapper smartToiletScheduleMapper;
+
+    @Override
+    public List<Map> findToiletScheduleList(Long fieldId, Date workDate) {
+        return smartToiletScheduleMapper.findToiletScheduleList(fieldId,workDate);
+    }
+
+    @Override
+    public int insert(TblSmartToiletSchedule tblSmartToiletSchedule) {
+        tblSmartToiletSchedule.setId(remoteIdProducerService.nextId());
+        tblSmartToiletSchedule.setCreateTime(DateUtils.getNowDate());
+        tblSmartToiletSchedule.setCreateUserId(SecurityUtils.getUserId());
+        return smartToiletScheduleMapper.insert(tblSmartToiletSchedule);
+    }
+
+    @Override
+    public int update(TblSmartToiletSchedule tblSmartToiletSchedule) {
+        tblSmartToiletSchedule.setUpdateTime(DateUtils.getNowDate());
+        tblSmartToiletSchedule.setCreateUserId(SecurityUtils.getUserId());
+        return smartToiletScheduleMapper.updateByPrimaryKey(tblSmartToiletSchedule);
+    }
+
+    @Override
+    public List<Map> getToiletType() {
+        return smartToiletScheduleMapper.getToiType();
+    }
+
+    @Override
+    public TblSmartToiletSchedule findById(Long id) {
+        TblSmartToiletSchedule tblSmartToiletSchedule = smartToiletScheduleMapper.selectByPrimaryKey(id);
+        return tblSmartToiletSchedule;
+    }
 }
