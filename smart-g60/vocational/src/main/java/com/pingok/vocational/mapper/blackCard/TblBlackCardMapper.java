15c4b73ecff502f020836696e4734163970bac80
lal <3055344019@qq.com>
2022-11-03 21:49:22 +0800

    狀態名單-接口编写

--- smart-g60/vocational/src/main/java/com/pingok/vocational/mapper/blackCard/TblBlackCardMapper.java
+++ smart-g60/vocational/src/main/java/com/pingok/vocational/mapper/blackCard/TblBlackCardMapper.java
@@ -12,30 +12,24 @@ import java.util.Map;
 public interface TblBlackCardMapper extends CommonRepository<TblBlackCard> {
 
     @Select({"<script>" +
-            "select a.ID as \"id\", a.MEDIA_ID as \"mediaId\" ," +
-            "a.ISSUER_ID as \"issuerId\" , a.INSERT_TIME as \"insertTime\"," +
-            "c.DICT_LABEL as \"type\"," +
+            "select a.ID as \"id\", a.MEDIA_ID as \"mediaId\" ,a.CARD_ID as \"cardId\"," +
+            "a.ISSUER_ID as \"issuerId\" , to_char(a.INSERT_TIME,'yyyy-mm-dd hh24:mi:ss') as \"insertTime\"," +
+//            "c.DICT_LABEL as \"type\"," +
             "case when a.STATUS = 1 then '进入状态名单' when a.STATUS = 2 then '解除状态名单' end as \"status\"," +
-            "a.CREATION_TIME as \"creationTime\" , a.VERSION as \"version\"," +
+            "to_char(a.CREATION_TIME,'yyyy-mm-dd hh24:mi:ss') as \"creationTime\" , a.VERSION as \"version\"," +
             "to_char(a.UPDATE_TIME,'yyyy-mm-dd hh24:mi:ss') as \"updateTime\" , a.MEDIA_TYPE as \"mediaType\", " +
             "a.APPLY_TIME as \"applyTime\" " +
             "from TBL_BLACK_CARD a " +
-            "<when test='mediaType == 1'> " +
-            "left join SYS_DICT_DATA c on c.DICT_VALUE = to_char(a.TYPE) and c.DICT_TYPE = 'blackcard_obu_type' " +
-            "</when>" +
-            "<when test='mediaType == 2'> " +
-            "left join SYS_DICT_DATA c on c.DICT_VALUE = to_char(a.TYPE) and c.DICT_TYPE = 'blackcard_cpc_type' " +
-            "</when>" +
-            "where 1=1 and a.MEDIA_TYPE = #{mediaType}" +
-            "<when test='mediaId != null'> " +
-            " and a.MEDIA_ID like CONCAT(#{mediaId}, '%') " +
+//            "<when test='mediaType == 1'> " +
+//            "left join SYS_DICT_DATA c on c.DICT_VALUE = to_char(a.TYPE) and c.DICT_TYPE = 'blackcard_obu_type' " +
+//            "</when>" +
+//            "<when test='mediaType == 2'> " +
+//            "left join SYS_DICT_DATA c on c.DICT_VALUE = to_char(a.TYPE) and c.DICT_TYPE = 'blackcard_cpc_type' " +
+//            "</when>" +
+            "where 1=1" +
+            "<when test='cardId != null'> " +
+            " and a.CARD_ID like '%' || #{cardId} || '%' " +
             "</when>"+
-            "<when test='startDate != null'> " +
-            " and a.CREATION_TIME &gt;= #{startDate} " +
-            "</when>"+
-            "<when test='endDate != null'> " +
-            " and a.CREATION_TIME &lt;= #{endDate} " +
-            "</when>" +
             "</script>"})
-    List<Map> selectList(@Param("mediaId") String mediaId, @Param("mediaType") Integer mediaType, @Param("startDate") String startDate, @Param("endDate")  String endDate);
+    List<Map> selectList(@Param("cardId") String cardId);
 }
