98a6adc1cf064d7d1c84ed12dbbdd8546fba3635
lal <3055344019@qq.com>
2022-11-14 17:33:42 +0800

    获取收费站车道列表接口改为树形结构，以及门架补充及时率

---
+++ smart-g60/monitor/src/main/java/com/pingok/monitor/domain/lane/LaneInfo.java
@@ -0,0 +1,13 @@
+package com.pingok.monitor.domain.lane;
+
+import lombok.Data;
+
+/**
+ * @author lal
+ */
+@Data
+public class LaneInfo {
+    private String laneName;
+    private String laneHex;
+//    private Integer status;
+}
