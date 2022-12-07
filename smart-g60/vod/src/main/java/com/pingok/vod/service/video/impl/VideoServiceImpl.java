df9d56be8db1d82a6a29f3c471666695b8451030
lal <3055344019@qq.com>
2022-12-07 15:53:02 +0800

    获取视频接口请求参数修改

--- smart-g60/vod/src/main/java/com/pingok/vod/service/video/impl/VideoServiceImpl.java
+++ smart-g60/vod/src/main/java/com/pingok/vod/service/video/impl/VideoServiceImpl.java
@@ -53,6 +53,9 @@ public class VideoServiceImpl implements IVideoService {
         JSONObject jsonBody = new JSONObject();
         jsonBody.put("cameraIndexCode", deviceId);//监控点唯一标识
         jsonBody.put("protocol", "hls");//取流协议
+        jsonBody.put("streamType", 0);//码流类型 0:主码 1:子码 2:第三码流
+        jsonBody.put("transmode", 1);//传输协议（传输层协议），0:UDP 1:TCP
+        jsonBody.put("streamform", "ps");//输出码流转封装格式
         String body = jsonBody.toJSONString();
         String result = null;
         try {
