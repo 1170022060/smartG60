c8789cc3706c1e180ebd7ea610150aa103187472
lal <3055344019@qq.com>
2022-12-16 17:53:02 +0800

    咨询信息管理页添加导出接口

--- smart-g60/vocational/src/main/java/com/pingok/vocational/controller/customer/CustomerProblemsController.java
+++ smart-g60/vocational/src/main/java/com/pingok/vocational/controller/customer/CustomerProblemsController.java
@@ -1,7 +1,10 @@
 package com.pingok.vocational.controller.customer;
 
+import com.pingok.vocational.domain.customer.CustomerProblemsEx;
 import com.pingok.vocational.domain.customer.TblCustomerProblems;
+import com.pingok.vocational.domain.customer.vo.CustomerProblemsVo;
 import com.pingok.vocational.service.customer.ICustomerProblemsService;
+import com.ruoyi.common.core.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.web.controller.BaseController;
 import com.ruoyi.common.core.web.domain.AjaxResult;
 import com.ruoyi.common.core.web.page.TableDataInfo;
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -40,26 +44,34 @@ public class CustomerProblemsController extends BaseController {
 //    @RequiresPermissions("vocational:customerProblems:info")
     @Log(title = "客户投诉与咨询记录-分页查询", businessType = BusinessType.OTHER)
     @GetMapping(value="/info")
-    public TableDataInfo info(@RequestParam(name = "handleUserId",required = false) Long handleUserId, @RequestParam(name = "startTime",required = false) Date startTime, @RequestParam(name = "endTime",required = false) Date endTime,@RequestParam(name = "handleDept",required = false) Long handleDept)
-    {
+    public TableDataInfo info(CustomerProblemsVo customerProblemsVo){
         startPage();
-        List<Map> info = customerProblemsService.selectCustomerProblems(handleUserId, startTime, endTime ,handleDept);
+        List<CustomerProblemsEx> info = customerProblemsService.selectCustomerProblems(customerProblemsVo);
         return getDataTable(info);
     }
 
-//    @RequiresPermissions("vocational:customerProblems:add")
-    @Log(title = "客户投诉与咨询记录", businessType = BusinessType.INSERT)
+    @RequiresPermissions("vocational:customerProblems:add")
+    @Log(title = "新增客户投诉与咨询", businessType = BusinessType.INSERT)
     @PostMapping
     public AjaxResult add(@Validated @RequestBody TblCustomerProblems tblCustomerProblems)
     {
         return toAjax(customerProblemsService.insertCustomer(tblCustomerProblems));
     }
 
-//    @RequiresPermissions("vocational:customerProblems:edit")
-    @Log(title = "客户投诉与咨询记录", businessType = BusinessType.UPDATE)
+    @RequiresPermissions("vocational:customerProblems:edit")
+    @Log(title = "客户投诉与咨询回复", businessType = BusinessType.UPDATE)
     @PutMapping
     public AjaxResult edit(@Validated @RequestBody TblCustomerProblems tblCustomerProblems)
     {
         return toAjax(customerProblemsService.updateCustomer(tblCustomerProblems));
+    }
+
+    @Log(title = "咨询管理",businessType = BusinessType.EXPORT)
+    @RequiresPermissions("vocational:customerProblems:export")
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, CustomerProblemsVo customerProblemsVo){
+        List<CustomerProblemsEx> list=customerProblemsService.selectCustomerProblems(customerProblemsVo);
+        ExcelUtil<CustomerProblemsEx> util = new ExcelUtil<>(CustomerProblemsEx.class);
+        util.exportExcel(response,list,"咨询投诉单");
     }
 }
