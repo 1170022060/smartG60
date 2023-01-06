af1031959875340b15cb0fa53f7a31b1b82dda0a
lal <3055344019@qq.com>
2023-01-06 15:00:08 +0800

    养护车辆接口编写

--- smart-g60/vocational/src/main/java/com/pingok/vocational/controller/maintenance/MaintainCarGpsController.java
+++ smart-g60/vocational/src/main/java/com/pingok/vocational/controller/maintenance/MaintainCarGpsController.java
@@ -2,16 +2,20 @@ package com.pingok.vocational.controller.maintenance;
 
 import com.pingok.vocational.domain.maintenance.TblMaintainCarGps;
 import com.pingok.vocational.service.maintenance.IMaintainCarGpsService;
+import com.ruoyi.common.core.web.controller.BaseController;
 import com.ruoyi.common.core.web.domain.AjaxResult;
+import com.ruoyi.common.core.web.page.TableDataInfo;
 import com.ruoyi.common.log.annotation.Log;
 import com.ruoyi.common.log.enums.BusinessType;
 import com.ruoyi.common.security.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 养护车辆GPS信息
@@ -20,16 +24,24 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/maintainCar")
-public class MaintainCarGpsController {
+public class MaintainCarGpsController extends BaseController {
     @Autowired
     private IMaintainCarGpsService maintainCarGpsService;
 
-    @RequiresPermissions("vocational:maintainCar:all")
+//    @RequiresPermissions("vocational:maintainCar:all")
     @Log(title = "养护车辆GPS信息-全部查询", businessType = BusinessType.OTHER)
     @GetMapping(value="/all")
     public AjaxResult all()
     {
         List<TblMaintainCarGps> info = maintainCarGpsService.selectAll();
         return AjaxResult.success(info);
+    }
+
+    @Log(title = "养护车辆信息-分页查询",businessType = BusinessType.OTHER)
+    @GetMapping
+    public TableDataInfo getInfo(@RequestParam(name = "vehPlate",required = false) String vehPlate){
+        startPage();
+        List<Map> info = maintainCarGpsService.selectInfo(vehPlate);
+        return getDataTable(info);
     }
 }
--- smart-g60/vocational/src/main/java/com/pingok/vocational/service/maintenance/IMaintainCarGpsService.java
+++ smart-g60/vocational/src/main/java/com/pingok/vocational/service/maintenance/IMaintainCarGpsService.java
@@ -3,6 +3,7 @@ package com.pingok.vocational.service.maintenance;
 import com.pingok.vocational.domain.maintenance.TblMaintainCarGps;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 养护车辆GPS信息表 业务层
@@ -17,4 +18,6 @@ public interface IMaintainCarGpsService {
      * @return 所有养护车辆GPS信息
      */
     List<TblMaintainCarGps> selectAll();
+
+    List<Map> selectInfo(String vehPlate);
 }
