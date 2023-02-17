53421c8e3467f2754f6dc7b6953805acbeecd7a0
lal <3055344019@qq.com>
2023-02-17 14:49:05 +0800

    两客一危GPS入库接口修改

---
+++ smart-g60/external-system-communication/src/main/java/com/pingok/external/mapper/primary/TblPrimaryGpsLogMapper.java
@@ -0,0 +1,8 @@
+package com.pingok.external.mapper.primary;
+
+import com.pingok.external.domain.primary.TblPrimaryGpsInfo;
+import com.pingok.external.domain.primary.TblPrimaryGpsInfoLog;
+import com.ruoyi.common.core.mapper.CommonRepository;
+
+public interface TblPrimaryGpsLogMapper extends CommonRepository<TblPrimaryGpsInfoLog> {
+}
