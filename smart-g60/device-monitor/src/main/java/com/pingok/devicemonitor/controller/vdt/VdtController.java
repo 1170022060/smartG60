73c293eeb120d210e1aec2d9d2a270a69798f72d
lal <3055344019@qq.com>
2023-03-06 10:54:44 +0800

    车检器流量统计定时任务

--- smart-g60/device-monitor/src/main/java/com/pingok/devicemonitor/controller/vdt/VdtController.java
+++ smart-g60/device-monitor/src/main/java/com/pingok/devicemonitor/controller/vdt/VdtController.java
@@ -1,15 +1,12 @@
 package com.pingok.devicemonitor.controller.vdt;
 
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.pingok.devicemonitor.service.vdt.IVdtService;
+import com.ruoyi.common.core.utils.DateUtils;
 import com.ruoyi.common.core.web.domain.AjaxResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -18,6 +15,12 @@ public class VdtController {
 
     @Autowired
     private IVdtService iVdtService;
+
+    @GetMapping
+    private AjaxResult collect() {
+        iVdtService.collect();
+        return AjaxResult.success();
+    }
 
     @PostMapping("/notifyResult")
     public AjaxResult notifyResult(@RequestBody JSONArray result) {
